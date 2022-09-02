2b318b606e793088431d8901436a52b8d288384a
Igor3550 <igormeloigormelo@gmail.com>
2022-09-02 18:36:00 -0300

    POST: /participants

--- src/app.js
+++ src/app.js
@@ -37,6 +37,53 @@ const messagesSchema = joi.object({
   time: joi.string().required()
 })
 
+app.post('/participants', async (req, res) => {
+
+  const participant = {
+    name: req.body.name
+  }
+
+  const validation = participantSchema.validate(participant)
+
+  if(validation.error) {
+    console.log(validation.error.details);
+    res.status(422).send(validation.error.details[0].message)
+    return;
+  }
+
+  try {
+
+    const searchedParticipant = await db.collection('participants').find(participant).toArray();
+    console.log(searchedParticipant);
+
+    if(searchedParticipant.length > 0){
+      res.sendStatus(409);
+      return;
+    }
+
+    await db.collection('participants').insertOne({
+      ...participant,
+      lastStatus: Date.now()
+    });
+
+    let timeNow = `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`;
+
+    await db.collection('messages').insertOne({
+      from: participant.name, 
+      to: 'Todos', 
+      text: 'entra na sala...', 
+      type: 'status', 
+      time: timeNow
+    })
+
+    res.sendStatus(201)
+
+  } catch (error) {
+    console.log(error)
+    res.sendStatus(500)
+  }
+})
+
 app.post('/status', (req, res) => {
   res.send('status')
 })
